85dd993ed3d8454a4bf83546f78064d684c5c22d
alexpehu <mamanipari@gmail.com>
2026-05-08 13:00:14 -0500

    Ejemplo CD-cd

--- SysAlmacen/src/main/java/pe/edu/upeu/sysalmacen/ServletInitializer.java
+++ SysAlmacen/src/main/java/pe/edu/upeu/sysalmacen/ServletInitializer.java
@@ -4,7 +4,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 public class ServletInitializer extends SpringBootServletInitializer {
-
+//Prueba de CI con Jenkins CD -Tomcat
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(SysAlmacenApplication.class);
